3f54c9096f7d37fd910729ee2e4c03aad9e48486
enp1s0 <ootomo.h.x86@gmail.com>
2024-07-23 10:13:18 +0900

    Update all

--- src/main.cu
+++ src/main.cu
@@ -1,123 +1,192 @@
 #include <iostream>
-#include <cublas.h>
 #include <cublas_v2.h>
 #include <chrono>
+#include <type_traits>
+#include <string>
 
 constexpr unsigned num_test = 128;
 
-enum gemm_mode_t {
-	sgemm,
-	cgemm,
-	unknown
+template <class T>
+inline cudaDataType get_cuda_data_type();
+template <>
+inline cudaDataType get_cuda_data_type<float>() {return CUDA_R_32F;};
+template <>
+inline cudaDataType get_cuda_data_type<cuComplex>() {return CUDA_C_32F;};
+template <>
+inline cudaDataType get_cuda_data_type<double>() {return CUDA_R_64F;};
+template <>
+inline cudaDataType get_cuda_data_type<cuDoubleComplex>() {return CUDA_C_32F;};
+template <>
+inline cudaDataType get_cuda_data_type<half>() {return CUDA_R_16F;};
+
+template <class T>
+inline T one() {return 1;}
+template <>
+inline cuComplex one<cuComplex>() {return make_cuComplex(1, 0);}
+template <>
+inline cuDoubleComplex one<cuDoubleComplex>() {return make_cuDoubleComplex(1, 0);}
+
+inline std::string get_op_str(const cublasOperation_t op) {
+  switch(op) {
+    case CUBLAS_OP_N:
+      return "N";
+    case CUBLAS_OP_T:
+      return "T";
+    case CUBLAS_OP_C:
+      return "C";
+    default:
+      return "Unknown";
+  }
+}
+
+struct run_gemm_base {
+  virtual void operator()(
+      cublasOperation_t op_a,
+      cublasOperation_t op_b,
+      const std::size_t m,
+      const std::size_t n,
+      const std::size_t k,
+      const std::size_t lda,
+      const std::size_t ldb,
+      const std::size_t ldc
+      ) = 0;
 };
+
+template <class T>
+struct run_gemm : run_gemm_base {
+  void operator() (
+      cublasOperation_t op_a,
+      cublasOperation_t op_b,
+      const std::size_t m,
+      const std::size_t n,
+      const std::size_t k,
+      const std::size_t lda,
+      const std::size_t ldb,
+      const std::size_t ldc
+      ) override {
+    const auto mat_a_size = lda * (op_a == CUBLAS_OP_N ? k : m);
+    const auto mat_b_size = ldb * (op_b == CUBLAS_OP_N ? n : k);
+    const auto mat_c_size = ldc * n;
+
+    T *mat_a, *mat_b, *mat_c;
+    cudaMalloc(&mat_a, mat_a_size * sizeof(T));
+    cudaMalloc(&mat_b, mat_b_size * sizeof(T));
+    cudaMalloc(&mat_c, mat_c_size * sizeof(T));
+
+    cudaMemset(mat_a, 0xee, mat_a_size * sizeof(T));
+    cudaMemset(mat_b, 0xee, mat_b_size * sizeof(T));
+    cudaMemset(mat_c, 0xee, mat_c_size * sizeof(T));
+
+    cublasHandle_t cublas_handle;
+    cublasCreate(&cublas_handle);
+
+    cudaDeviceSynchronize();
+    const auto start_clock = std::chrono::system_clock::now();
+
+    const T alpha = one<T>(), beta = one<T>();
+
+    for (unsigned t = 0; t < num_test; t++) {
+      cublasGemmEx(
+          cublas_handle,
+          op_a,
+          op_b,
+          m, n, k,
+          &alpha,
+          mat_a, get_cuda_data_type<T>(), lda,
+          mat_b, get_cuda_data_type<T>(), ldb,
+          &beta,
+          mat_c, get_cuda_data_type<T>(), ldc,
+          get_cuda_data_type<T>(),
+          CUBLAS_GEMM_DEFAULT
+          );
+    }
+
+    cudaDeviceSynchronize();
+    const auto end_clock = std::chrono::system_clock::now();
+
+    const auto elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_clock - start_clock).count() * 1e-9;
+    auto complexity = 2lu * m * n * k;
+
+    if (std::is_same_v<cuComplex, T> || std::is_same_v<cuDoubleComplex, T>) {
+      complexity *= 4;
+    }
+
+    std::printf("op_A=%s, op_B=%s, shape=(%lu, %lu, %lu), ld=(%lu, %lu, %lu), throughput=%e TFlop/s\n",
+                get_op_str(op_a).c_str(),
+                get_op_str(op_b).c_str(),
+                m, n, k,
+                lda, ldb, ldc,
+                complexity * num_test / elapsed_time * 1e-12
+               );
+
+    cublasDestroy(cublas_handle);
+
+    cudaFree(mat_a);
+    cudaFree(mat_b);
+    cudaFree(mat_c);
+  }
+};
+
+inline cublasOperation_t get_op(const std::string s) {
+  if (s == "N" || s == "n") {
+    return CUBLAS_OP_N;
+  } else if (s == "T" || s == "t") {
+    return CUBLAS_OP_T;
+  } else if (s == "C" || s == "c") {
+    return CUBLAS_OP_C;
+  } else {
+    throw std::runtime_error("Unknown op: " + s);
+  }
+}
 
 // ./cublas.out sgemm m n k
 int main(int argc, char** argv) {
-	if (argc < 5) {
-		std::fprintf(stderr,
-				"%s [gemm type] [m] [n] [k]\n"
-				" - [gemm type] : sgemm / cgemm\n",
-				argv[0]
-				);
-		return 1;
-	}
+  if (argc < 7) {
+    std::fprintf(stderr,
+                 "%s [gemm type] [op_A] [op_B] [m] [n] [k] (optional: [lda] [ldb] [ldc])\n"
+                 " - [gemm type] : s | c | d | z | h\n"
+                 " - [op_A/B] : N | T | C\n",
+                 argv[0]
+                );
+    return 1;
+  }
 
-	gemm_mode_t gemm_mode = unknown;
-	std::string gemm_mode_str = argv[1];
-	if (gemm_mode_str == "sgemm") {
-		gemm_mode = sgemm;
-	} else if (gemm_mode_str == "cgemm") {
-		gemm_mode = cgemm;
-	}
+  const std::string gemm_mode_str = argv[1];
 
-	if (gemm_mode == unknown) {
-		std::fprintf(stderr, "Unknown gemm type : %s\n", gemm_mode_str.c_str());
-		return 1;
-	}
+  const auto op_a = get_op(argv[2]);
+  const auto op_b = get_op(argv[3]);
+  const auto m = std::stoul(argv[4]);
+  const auto n = std::stoul(argv[5]);
+  const auto k = std::stoul(argv[6]);
 
-	const auto m = std::stoul(argv[2]);
-	const auto n = std::stoul(argv[3]);
-	const auto k = std::stoul(argv[4]);
+  const auto lda = (argc >= 8) ? std::stoul(argv[7]) : (op_a == CUBLAS_OP_N ? m : k);
+  const auto ldb = (argc >= 9) ? std::stoul(argv[8]) : (op_b == CUBLAS_OP_N ? k : n);
+  const auto ldc = (argc >= 10) ? std::stoul(argv[9]) : m;
 
-	auto mat_a_size = m * k;
-	auto mat_b_size = k * n;
-	auto mat_c_size = m * n;
+  run_gemm_base* gemm = nullptr;
+  if (gemm_mode_str == "s") {
+    gemm = new run_gemm<float>;
+  } else if (gemm_mode_str == "c") {
+    gemm = new run_gemm<cuComplex>;
+  } else if (gemm_mode_str == "d") {
+    gemm = new run_gemm<double>;
+  } else if (gemm_mode_str == "z") {
+    gemm = new run_gemm<cuDoubleComplex>;
+  } else if (gemm_mode_str == "h") {
+    gemm = new run_gemm<half>;
+  } else {
+    std::fprintf(stderr, "Unknown gemm type : %s\n", gemm_mode_str.c_str());
+    return 1;
+  }
 
-	if (gemm_mode == cgemm) {
-		mat_a_size *= 2;
-		mat_b_size *= 2;
-		mat_c_size *= 2;
-	}
+  (*gemm)(
+      op_a,
+      op_b,
+      m, n, k,
+      lda,
+      ldb,
+      ldc
+      );
 
-	float *mat_a, *mat_b, *mat_c;
-	cudaMalloc(&mat_a, mat_a_size * sizeof(float));
-	cudaMalloc(&mat_b, mat_b_size * sizeof(float));
-	cudaMalloc(&mat_c, mat_c_size * sizeof(float));
-
-	cublasHandle_t cublas_handle;
-	cublasCreate(&cublas_handle);
-
-	const auto sgemm_func = [&]() {
-		const float alpha = 1.0f, beta = 0.0f;
-		cublasSgemm(
-				cublas_handle,
-				CUBLAS_OP_T, CUBLAS_OP_T,
-				m, n, k,
-				&alpha,
-				mat_a, k,
-				mat_b, k,
-				&beta,
-				mat_c, m
-				);
-	};
-	const auto cgemm_func = [&]() {
-		const cuComplex alpha = make_cuComplex(1.0f, 00.f), beta = make_cuComplex(0.0f, 0.0f);
-		cublasCgemm(
-				cublas_handle,
-				CUBLAS_OP_T, CUBLAS_OP_T,
-				m, n, k,
-				&alpha,
-				reinterpret_cast<cuComplex*>(mat_a), k,
-				reinterpret_cast<cuComplex*>(mat_b), k,
-				&beta,
-				reinterpret_cast<cuComplex*>(mat_c), m
-				);
-	};
-
-	if (gemm_mode == sgemm) {
-		sgemm_func();
-	} else {
-		cgemm_func();
-	}
-
-	cudaDeviceSynchronize();
-	const auto start_clock = std::chrono::system_clock::now();
-
-	for (unsigned t = 0; t < num_test; t++) {
-		if (gemm_mode == sgemm) {
-			sgemm_func();
-		} else {
-			cgemm_func();
-		}
-	}
-
-	cudaDeviceSynchronize();
-	const auto end_clock = std::chrono::system_clock::now();
-
-	const auto elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_clock - start_clock).count() * 1e-9;
-	auto complexity = 2lu * m * n * k;
-	if (gemm_mode == cgemm) {
-		complexity *= 4;
-	}
-
-	std::printf("shape=(%lu, %lu, %lu), throughput=%e TFlop/s\n",
-			m, n, k,
-			complexity * num_test / elapsed_time * 1e-12
-			);
-
-	cublasDestroy(cublas_handle);
-
-	cudaFree(mat_a);
-	cudaFree(mat_b);
-	cudaFree(mat_c);
+  delete gemm;
 }
